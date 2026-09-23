32658c7171c8d6cef1a74e3f74b5f954c57c92d4
stepanPise <stepan.pise@gmail.com>
2026-09-23 12:04:08 +0200

    Initial commit

---
+++ src/main/java/com/trixi/pise_trixi/PiseTrixiApplication.java
@@ -0,0 +1,13 @@
+package com.trixi.pise_trixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiseTrixiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiseTrixiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trixi/pise_trixi/PiseTrixiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trixi.pise_trixi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiseTrixiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
